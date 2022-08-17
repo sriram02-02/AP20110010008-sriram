ba336ff982da03204aefb8632e750dc6e5083caf
Sai Sriram Reddy Kolli <91004206+sriram02-02@users.noreply.github.com>
2022-08-17 20:48:22 +0530

    Add files via upload

---
+++ cd2.py
@@ -0,0 +1,37 @@
+x=input()
+y=['a','b']
+for i in x:
+  if i not in y:
+     print('string not accepted')
+     exit()
+p=0
+for i in x:
+   if p==0:
+     if i=='a':
+       p=1
+     else:
+        p=2
+   elif p==1:
+       if i=='a':
+          p=3
+       else:
+           p=2
+   elif p==2:
+          if i=='a':
+            p=3
+          else:
+            p=4
+   elif p==3:
+         if i=='a':
+             p=3
+         else:
+             p=2
+   elif p==4:
+         if i=='a':
+            p=1
+         else:
+            p=4
+if p==4 or p==3:
+   print('string accepted')
+else:
+    print('string not accepted')
